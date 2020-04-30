8ddeb44d077b85d2673222be17c3c469ee717928
dmaliuta <malyutadima@gmail.com>
2020-04-30 11:35:38 +0300

    Gitignore Add date/time to screenshots name

--- src/test/java/SpinTests/Utility.java
+++ src/test/java/SpinTests/Utility.java
@@ -6,20 +6,26 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Utility {
     public static void captureScreenshot(WebDriver driver, String screenshotName)
     {
         try
         {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = new Date();
             TakesScreenshot ts = (TakesScreenshot)driver;
             File source = ts.getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(source, new File("/Users/dmalyuta/IdeaProjects/SpinBackup/src/test/java/screenshots/" + screenshotName + ".png"));
+            FileUtils.copyFile(source, new File("/Users/dmalyuta/IdeaProjects/SpinBackup/src/test/java/screenshots/" + screenshotName + " " + dateFormat.format(date) + ".png"));
             System.out.println("Screenshot captured");
         }
         catch (Exception e)
         {
-            System.out.println("Exception while taken screenshot" + e.getMessage());
+
+            System.out.println("Exception while taken screenshot " + e.getMessage() );
         }
 
 
